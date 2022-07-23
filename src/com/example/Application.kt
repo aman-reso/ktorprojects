da07aea26c8f45702b65e1385bf2210057b806aa
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2022-07-23 17:04:09 +0530

    http

---
+++ src/com/example/Application.kt
@@ -0,0 +1,24 @@
+package com.example
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import com.example.plugins.configureRouting
+
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
+data class MySession(val count: Int = 0)
--- src/com/example/plugins/Routing.kt
+++ src/com/example/plugins/Routing.kt
@@ -1,4 +1,4 @@
-package main.com.example.plugins
+package com.example.plugins
 
 import io.ktor.server.routing.*
 import io.ktor.http.*
@@ -7,13 +7,10 @@ import io.ktor.server.response.*
 import io.ktor.server.request.*
 
 fun Application.configureRouting() {
-
-    // Starting point for a Ktor app:
     routing {
         get("/") {
-            call.respondText("Hello World!")
+
         }
     }
-    routing {
-    }
+
 }
